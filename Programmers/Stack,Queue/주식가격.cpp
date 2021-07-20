286884e371a23ac57e5b8edc3732fef5c9a39bc6
GiHongPark-Kor <72617914+GiHongPark-Kor@users.noreply.github.com>
2021-07-20 18:03:09 +0900

    Coding test practice
    난이도 : level 2

---
+++ Programmers/Stack,Queue/주식가격.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<int> prices)
+{
+    vector<int> answer;
+    for (int i = 0; i < prices.size() - 1; i++)
+    {
+        int count = 0;
+        for (int j = i; j < prices.size() - 1; j++)
+        {
+            if (prices[i] <= prices[j])
+                count++;
+            else
+                break;
+        }
+        answer.push_back(count);
+    }
+    answer.push_back(0);
+    return answer;
+}
