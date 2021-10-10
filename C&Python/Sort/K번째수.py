f3464a8e483c7ca1b3a4c6d877182bce7384e58f
GihongPark <gihong2922@gmail.com>
2021-10-10 23:57:40 +0900

    Create K번째수.py (programmers)
    난이도 : level 1

---
+++ C&Python/Sort/K번째수.py
@@ -0,0 +1,7 @@
+﻿def solution(array, commands):
+    answer = []
+    for i in range(len(commands)):
+        sub_str = array[(commands[i][0] - 1):commands[i][1]]
+        sub_str.sort()
+        answer.append((sub_str[commands[i][2] - 1]))
+    return answer
