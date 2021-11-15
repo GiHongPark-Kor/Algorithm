5fce4d994d3da122b9249b8e1a0d69630168b384
GihongPark <gihong2922@gmail.com>
2021-11-15 18:39:29 +0900

    Update 밥.py
    change explanation

--- C&Python/Greedy/밥.py
+++ C&Python/Greedy/밥.py
@@ -5,14 +5,14 @@ for i in range(N):
     a, b = map(int, input().split())
     arr.append([a, b])
 
-# sort : 차이가 가장 작은 순으로 정렬 : 가성비가 가장 좋은 5000원을 선택한다.
+# sort : 차이가 가장 큰 순으로 정렬 : 가성비가 가장 좋은 5000원을 선택한다.
 arr = sorted(arr, key = lambda x : abs(x[0] - x[1]), reverse = True)
 answer = 0
 
 
 # calculate
 for i in range(N):
-    # 현재 남은 개수 * 가격 < 5000 : 무조건 1000원 선택
+    # 현재 돈 - 현재 남은 개수 * 1000 < 5000 : 무조건 1000원 선택
     if X - (N - i - 1) * 1000 < 5000:
         X -= 1000
         answer += arr[i][1]
