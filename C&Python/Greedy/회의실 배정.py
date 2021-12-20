4b3bc329bb08a5f81290fc415c470769e8fe514c
GihongPark <gihong2922@gmail.com>
2021-12-21 00:20:52 +0900

    Update 회의실 배정.py
    change sort

--- C&Python/Greedy/회의실 배정.py
+++ C&Python/Greedy/회의실 배정.py
@@ -2,14 +2,15 @@ if __name__ == "__main__":
     # input
     N = int(input())
     arr = []
-    for i in range(N):
+    for _ in range(N):
         s, e = map(int, input().split())
         arr.append([s,e])
 
-    arr = sorted(arr, key = lambda x : x[1])
+    # 예외 테스트 케이스 : (1, 2) (2, 2) -> s 정렬도 필요!!
+    arr = sorted(arr, key = lambda x : (x[1],x[0]))
     temp = -1
     answer = 0
-    
+
     for i in range(len(arr)):
         if temp > arr[i][0]:
             pass
